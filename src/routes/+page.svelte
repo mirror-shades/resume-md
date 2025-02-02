387d0a32d5736400e1876a57f15d6a8d72436265
mirror-shades <logan.dembicki@gmail.com>
2025-02-01 17:57:54 -0800

    daisy ui wokring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script>
     import "../styles/app.css";
+    export let data;
 </script>
 
 <p class="text-3xl font-bold underline">Hello world!</p>
+
+<button
+  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+  Button
+</button>
+
+<slot />
